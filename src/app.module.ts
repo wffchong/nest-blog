2ea92e2897ddc33a0309991e9c9e326325f3a7fb
wffchong <283953455@qq.com>
2022-11-13 14:59:57 +0800

    feat: 添加jwt策略

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common'
 import { PrismaModule } from './prisma/prisma.module'
 import { AuthModule } from './auth/auth.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-    imports: [PrismaModule, AuthModule],
-    controllers: [],
-    providers: []
+    imports: [
+        PrismaModule,
+        AuthModule,
+        ConfigModule.forRoot({
+            isGlobal: true
+        })
+    ]
 })
 export class AppModule {}
