3b7a6ccbd6370eccd9a95efa98a9468e827ced8d
wffchong <283953455@qq.com>
2022-11-14 15:05:01 +0800

    feat: 添加category迁移文件

---
+++ prisma/migrations/20221114065510_/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - Added the required column `categoryId` to the `article` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `article` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `role` to the `user` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `article` ADD COLUMN `categoryId` INTEGER UNSIGNED NOT NULL,
+    ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `user` ADD COLUMN `role` VARCHAR(191) NOT NULL;
+
+-- CreateTable
+CREATE TABLE `category` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `article` ADD CONSTRAINT `article_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `category`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 import { hash } from 'argon2'
+import _ from 'lodash'
 import { Random } from 'mockjs'
 
 const prisma = new PrismaClient()
@@ -8,15 +9,25 @@ async function run() {
     await prisma.user.create({
         data: {
             name: 'admin',
-            password: await hash('admin123')
+            password: await hash('admin123'),
+            role: 'admin'
         }
     })
+
+    for (let i = 1; i <= 5; i++) {
+        await prisma.category.create({
+            data: {
+                title: Random.ctitle(3, 6)
+            }
+        })
+    }
 
     for (let i = 0; i < 50; i++) {
         await prisma.article.create({
             data: {
                 title: Random.ctitle(10, 30),
-                content: Random.cparagraph(30, 50)
+                content: Random.cparagraph(30, 50),
+                categoryId: _.random(1, 5)
             }
         })
     }
