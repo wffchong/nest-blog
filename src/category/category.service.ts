74e4554a4307b4ca8aef1800c8baad0402e6317e
wffchong <283953455@qq.com>
2022-11-14 15:51:44 +0800

    feat: 添加文章栏目增删改查

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,26 +1,35 @@
+import { PrismaService } from '@/prisma/prisma.service'
 import { Injectable } from '@nestjs/common'
 import { CreateCategoryDto } from './dto/create-category.dto'
 import { UpdateCategoryDto } from './dto/update-category.dto'
 
 @Injectable()
 export class CategoryService {
-    create(createCategoryDto: CreateCategoryDto) {
-        return 'This action adds a new category'
+    constructor(private readonly prisma: PrismaService) {}
+
+    async create(createCategoryDto: CreateCategoryDto) {
+        return await this.prisma.category.create({
+            data: createCategoryDto
+        })
     }
 
-    findAll() {
-        return `This action returns all category`
+    async findAll() {
+        return await this.prisma.category.findMany()
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} category`
+    async findOne(id: number) {
+        return await this.prisma.category.findFirst({
+            where: {
+                id
+            }
+        })
     }
 
-    update(id: number, updateCategoryDto: UpdateCategoryDto) {
-        return `This action updates a #${id} category`
+    async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+        return await this.prisma.category.update({ where: { id }, data: updateCategoryDto })
     }
 
-    remove(id: number) {
-        return `This action removes a #${id} category`
+    async remove(id: number) {
+        return await this.prisma.category.delete({ where: { id } })
     }
 }
--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,1 +1,6 @@
-export class CreateCategoryDto {}
+import { IsNotEmpty } from 'class-validator'
+
+export class CreateCategoryDto {
+    @IsNotEmpty({ message: '类别名称不能为空' })
+    title: string
+}
