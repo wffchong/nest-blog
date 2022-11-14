d31a1d925308b4d755507bb00a64ada559820fc8
wffchong <283953455@qq.com>
2022-11-14 13:21:06 +0800

    feat: 设置api请求前缀

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
 import { AppModule } from './app.module'
 import Validate from './common/validata'
 import { TransformInterceptor } from './transform.interceptor'
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
+    const app = await NestFactory.create<NestExpressApplication>(AppModule)
     app.useGlobalPipes(new Validate())
     app.useGlobalInterceptors(new TransformInterceptor())
+    // 设置前缀
+    app.setGlobalPrefix('api')
     await app.listen(3000)
 }
 bootstrap()
