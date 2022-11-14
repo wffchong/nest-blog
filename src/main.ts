1615f5c79a1c21815dc7ae8cd1a99a30b2d86550
wffchong <283953455@qq.com>
2022-11-14 17:51:55 +0800

    feat: 设置静态资源目录

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
+import { join } from 'path'
 import { AppModule } from './app.module'
 import Validate from './common/validata'
 import { TransformInterceptor } from './transform.interceptor'
@@ -10,6 +11,10 @@ async function bootstrap() {
     app.useGlobalInterceptors(new TransformInterceptor())
     // 设置前缀
     app.setGlobalPrefix('api')
+    // 设置文件上传后的静态资源目录
+    app.useStaticAssets(join(__dirname, '../uploads'), {
+        prefix: '/uploads'
+    })
     await app.listen(3000)
 }
 bootstrap()
